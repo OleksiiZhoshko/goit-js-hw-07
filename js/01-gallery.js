a9c6c3386213171c1c9664ecbb2f4291c0b24b8b
OleksiyZ <106480026+OleksiyZ@users.noreply.github.com>
2022-10-30 15:02:48 +0200

    Дз №7 до здачі вилизав

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,44 +1,93 @@
-import { galleryItems } from './gallery-items.js';
+// друга спроба
 
-const refs = {
-	galleryRef: document.querySelector('.gallery'),
-};
+import { galleryItems } from "./gallery-items.js";
 
-refs.galleryRef.addEventListener('click', onClickGalleryItem);
+const galleryRef = document.querySelector(".gallery");
 
-function addgaleryItemsMarkup(itemsArr) {
-	return itemsArr
-		.map(({ original, preview, description }) => {
-			return `<div class="gallery__item"> <a class="gallery__link" href="${original}">
-		<img class="gallery__image" src="${preview}" data-source="${original}"
-			alt="${description}"/></a></div> `;
-		})
-		.join('');
+function createGalleryMarcap(arry) {
+  return arry
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item"> <a class="gallery__link" href="${original}">
+ 		<img class="gallery__image" src="${preview}" data-source="${original}"
+ 		alt="${description}"/></a></div> `;
+    })
+    .join("");
 }
 
-refs.galleryRef.insertAdjacentHTML(
-	'beforeend',
-	addgaleryItemsMarkup(galleryItems)
-);
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarcap(galleryItems));
+galleryRef.addEventListener("click", onClickImage);
 
-function onClickGalleryItem(evt) {
-	evt.preventDefault();
-	if (evt.target.nodeName !== 'IMG') {
-		return;
-	}
-	const originalImgEl = basicLightbox.create(`
-	<div class="modal">
-    	<img src="${evt.target.dataset.source}" width="1280">
-	</div>
-`);
-	originalImgEl.show();
+function onClickImage(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
 
-	refs.galleryRef.addEventListener('keydown', evt => {
-		if (evt.code === 'Escape') {
-			originalImgEl.close();
-		}
-	});
-};
+  const instance = basicLightbox.create(
+    `
+		<img src=${evt.target.dataset.source} 
+		alt=${evt.target.alt}
+		width="1280">		
+	`,
+    {
+      onShow: () => {
+        galleryRef.addEventListener("keydown", onEscapeKeyPress);
+      },
+      onClose: () => {
+        galleryRef.addEventListener("keydown", onEscapeKeyPress);
+      },
+    }
+  );
+  instance.show();
 
+  function onEscapeKeyPress(evt) {
+    if (evt.key === "Escape") {
+      instance.close();
+    }
+  }
+  galleryRef.addEventListener("keydown", onEscapeKeyPress);
+}
 
+// Перша спроба
 
+// import { galleryItems } from './gallery-items.js';
+
+// const refs = {
+// 	galleryRef: document.querySelector('.gallery'),
+// };
+
+// refs.galleryRef.addEventListener('click', onClickGalleryItem);
+
+// function addgaleryItemsMarkup(itemsArr) {
+// 	return itemsArr
+// 		.map(({ original, preview, description }) => {
+// 			return `<div class="gallery__item"> <a class="gallery__link" href="${original}">
+// 		<img class="gallery__image" src="${preview}" data-source="${original}"
+// 			alt="${description}"/></a></div> `;
+// 		})
+// 		.join('');
+// }
+
+// refs.galleryRef.insertAdjacentHTML(
+// 	'beforeend',
+// 	addgaleryItemsMarkup(galleryItems)
+// );
+
+// function onClickGalleryItem(evt) {
+// 	evt.preventDefault();
+// 	if (evt.target.nodeName !== 'IMG') {
+// 		return;
+// 	}
+// 	const originalImgEl = basicLightbox.create(`
+// 	<div class="modal">
+//     	<img src="${evt.target.dataset.source}" width="1280">
+// 	</div>
+// `);
+// 	originalImgEl.show();
+
+// 	refs.galleryRef.addEventListener('keydown', evt => {
+// 		if (evt.code === 'Escape') {
+// 			originalImgEl.close();
+// 		}
+// 	});
+// };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,51 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const refs = {
-	galleryRef: document.querySelector('.gallery'),
-};
+// Друга спроба
+import { galleryItems } from "./gallery-items.js";
 
-function addgaleryItemsMarkup(itemsArr) {
-	return itemsArr
-		.map(({ original, preview, description }) => {
-			return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
-		})
-		.join('');
+const galleryRef = document.querySelector(".gallery");
+
+function createGalleryMarcap(arry) {
+  return arry
+    .map(({ original, preview, description }) => {
+      return `<li class="gallery__item"> <a class="gallery__link" href="${original}">
+ 		<img class="gallery__image" src="${preview}" alt="${description}"/></a></li> `;
+    })
+    .join("");
 }
 
-console.log(addgaleryItemsMarkup(galleryItems));
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarcap(galleryItems));
 
-refs.galleryRef.insertAdjacentHTML(
-	'beforeend',
-	addgaleryItemsMarkup(galleryItems)
-);
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
 
-const galerySimpleLightBox = new SimpleLightbox('.gallery a', {
-	captionsData: 'alt',
-	loop: true,
-	captionDelay: 250,
-	animationSpeed: 250,
-	animationSlide: true,
-});
 
-galerySimpleLightBox.show('show.simplelightbox', function () {});
+// Перша спроба
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+// const refs = {
+// 	galleryRef: document.querySelector('.gallery'),
+// };
+
+// function addgaleryItemsMarkup(itemsArr) {
+// 	return itemsArr
+// 		.map(({ original, preview, description }) => {
+// 			return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+// 		})
+// 		.join('');
+// }
+
+// console.log(addgaleryItemsMarkup(galleryItems));
+
+// refs.galleryRef.insertAdjacentHTML(
+// 	'beforeend',
+// 	addgaleryItemsMarkup(galleryItems)
+// );
+
+// const galerySimpleLightBox = new SimpleLightbox('.gallery a', {
+// 	captionsData: 'alt',
+// 	loop: true,
+// 	captionDelay: 250,
+// 	animationSpeed: 250,
+// 	animationSlide: true,
+// });
+
+// galerySimpleLightBox.show('show.simplelightbox', function () {});
